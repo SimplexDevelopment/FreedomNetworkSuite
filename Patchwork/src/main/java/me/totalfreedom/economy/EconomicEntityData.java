6ccaf5eb2577adc55833fc9f7f517a6d0b3a9bdf
Allink <arclicious@vivaldi.net>
2023-06-09 17:22:54 +0100

    Add documentation to economy-related interfaces

--- Patchwork/src/main/java/me/totalfreedom/economy/EconomicEntityData.java
+++ Patchwork/src/main/java/me/totalfreedom/economy/EconomicEntityData.java
@@ -1,14 +1,40 @@
 package me.totalfreedom.economy;
 
+/**
+ * Metadata associated with a {@link EconomicEntity}
+ */
 public interface EconomicEntityData
 {
+    /***
+     * @return the transaction freeze state
+     */
     boolean areTransactionsFrozen();
 
+    /***
+     * @return the balance
+     */
     long getBalance();
 
+    /**
+     * Adds the provided amount to the associated instance's balance
+     *
+     * @param amount the amount to add
+     * @return the new balance
+     */
     long addToBalance(final long amount);
 
+    /**
+     * Subtracts the provided amount from the associated instance's balance
+     *
+     * @param amount the amount to subtract
+     * @return the new balance
+     */
     long removeFromBalance(final long amount);
 
+    /**
+     * Sets the balance of the associated instance
+     *
+     * @param newBalance the new balance
+     */
     void setBalance(final long newBalance);
 }
--- Patchwork/src/main/java/me/totalfreedom/economy/TransactionLogger.java
+++ Patchwork/src/main/java/me/totalfreedom/economy/TransactionLogger.java
@@ -1,6 +1,14 @@
 package me.totalfreedom.economy;
 
+/**
+ * A class that intercepts transactions after they are completed and logs them to a data point
+ */
 public interface TransactionLogger
 {
+    /**
+     * Logs a completed transaction
+     *
+     * @param completedTransaction the completed transaction to log
+     */
     void logTransaction(CompletedTransaction completedTransaction);
 }
